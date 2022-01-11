b1736a4b816681c22563e1891ed26f52745847af
OkonovUrmat <okonov.okonov@gmail.com>
2022-01-11 13:10:34 +0600

    Reveal data in tableView

--- Generic and Reusable calls/ViewController.swift
+++ Generic and Reusable calls/ViewController.swift
@@ -7,14 +7,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct User: Codable {
+    let name: String
+    let email: String
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    struct Constants {
+        static let usersURL = URL(string: "https://jsonplaceholder.typecode.com/users")
+    }
+    
+    var models = [Codable]()
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        view.addSubview(table)
+        table.delegate = self
+        table.dataSource = self
+        fetch()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        table.frame = view.bounds
+    }
+    
+    func fetch() {
+        URLSession.shared.request(url: Constants.usersURL, expecting: [User].self) { [weak self] result in
+            switch result {
+            case .success(let users):
+                DispatchQueue.main.async {
+                    self?.models = users
+                    self?.table.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = (models[indexPath.row] as? User)?.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
 }
 
 extension URLSession {
